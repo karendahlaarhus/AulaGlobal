f7aa7b33a12dbcd6da9a0a19f22809af4c906417
solveiglangas <solveig.langas@hotmail.com>
2021-10-28 12:52:53 +0200

    courses sql

---
+++ courses.sql
@@ -0,0 +1,46 @@
+USE `BD89_06_aulamaster`;
+
+INSERT INTO `Courses` (`id_course`, `name`, `description`,`academic_course`) VALUES (1, 
+'Computing technologies for the web', 
+'In this class you will learn about development of distributed components, 
+web programming, web application desgin patterns, Techniques for asynchronous 
+communications with servers, MVC architectures, Persistence engines and Web 
+development for specific platforms',
+'Computer Science and Engineering');
+
+INSERT INTO `Courses` (`id_course`, `name`, `description`,`academic_course`) VALUES (2,
+'Advanced Software Design',
+'Generic techniques for modeling, design and implementation of modular software, 
+focussing on industrial techniques and domains. Component-based design, 
+component-based architectures and frameworks, distributed component-based systems. 
+Model-driven development of software, code generation and domain-specific languages. 
+Testing, continuous integration, deployment and governance.',
+'Computer Science and Engineering');
+
+INSERT INTO `Courses` (`id_course`, `name`, `description`,`academic_course`) VALUES (3,
+'Managerial Economics and Accounting',
+'Managerial accounting: - Introduction to managerial accounting - 
+Costs and cost structures - Cost allocation - Costing - Short-term decision making 
+- Cost-, volume-, profitanalysis - Introduction to management science - Pricing decisions 
+- Long-term decisions Financial accounting: - Purpose and use of financial accounts 
+- Double entry bookkeeping - Basic bookkeeping - Introduction to accrual accounting 
+- Closing the books',
+'Buisness Administration');
+
+
+INSERT INTO `Courses` (`id_course`, `name`, `description`,`academic_course`) VALUES (4,
+'Marketing - Basic Course',
+'The course aims to provide students with key basic skills through a broad presentation 
+of the marketing discipline, develop students understanding of the topics presented and 
+to develop students ability to apply knowledge in realistic marketing situations. 
+There will also be an emphasis on developing a market-oriented attitude among students.',
+'Buisness Administration');
+
+INSERT INTO `Courses` (`id_course`, `name`, `description`,`academic_course`) VALUES (5,
+'Data Modelling, Database Managment Systems',
+'Basic introduction to data modelling with emphasis on Entitity Relationship modelling. 
+The relational model, relational algebra and SQL. Database construction. Normalization 
+as a design theory for relational databases. Other database models. Storage technologies, 
+file organization and index structures. Query processing. Database management systems 
+(DBMS). Transactions, concurrency and recovery. Data integrity.',
+'Computer Science and Engineering');
